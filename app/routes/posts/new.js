f3f3589ab6da4b1b904f4c50c34fa6f657025f03
Marcin Gajewski <vegijtha@gmail.com>
2016-03-24 15:20:39 +0100

    post new and edit actions

--- app/routes/posts/new.js
+++ app/routes/posts/new.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord('post', {});
+  },
+
   actions: {
     save: function(model) {
       model.save().then(() => this.transitionTo('posts.index'));
     }
   }
-});
+})
